f893ab3d9c221278addc032dd5304185ba55a222
GodVilan <srikanthreddynandireddy@gmail.com>
2023-03-19 22:36:26 +0530

    new

---
+++ frontend-react/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className = "container-fluid">
+      <h1>NANDIREDDY SRIKANTH REDDY</h1>
+    </div>
+  )
+}
+
+export default App
