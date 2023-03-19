70cb2fcc625dd77a047c8fe029b8c27d69f07909
GodVilan <srikanthreddynandireddy@gmail.com>
2023-03-19 23:21:50 +0530

    new

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout";
+import "./App.scss";
 
 function App() {
-
   return (
-    <div className = "container-fluid">
-      <h1>NANDIREDDY SRIKANTH REDDY</h1>
-    </div>
-  )
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}></Route>
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
--- server.js
+++ server.js
@@ -1,20 +1,4 @@
-// require("dotenv").config();
-
-// import dotenv from "dotenv";
-// dotenv.config();
-import cors from "cors";
-import { config } from "dotenv";
-config();
-
-// const todoLib = require("./backend/lib/todoLib");
-import * as todoLib from "./backend/lib/todoLib.js";
-
-// const mongoose = require("mongoose");
-import mongoose from "mongoose";
-
-// const express = require('express');
 import express, {request, response} from "express";
-// const { request } = require("express");
 
 const app = express();
 const port = process.env.PORT || 3010;
@@ -22,14 +6,12 @@ const options = {
 	extensions:['htm','html','css','js','ico','jpg','jpeg','png','svg','pdf'],
 	index:['index.html'],
 }
-
-app.use(cors());
 app.use(express.static("frontend"));
 app.use(express.json());
 
 
 app.get("/", function(req, res){
-	res.sendFile(process.cwd()+"/frontend/html/index.html");
+	res.sendFile(process.cwd()+"/frontend/index.html");
 });
 
 app.listen(port, function(){
